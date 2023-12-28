547fb0da9681f78ff4a335c78657c93cfc9961b4
Altafalam3 <altaf.alam0032@gmail.com>
2023-12-28 15:06:55 +0530

    Trying harder fix

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -4,32 +4,49 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true // Enabling secure uploads
-});
+  secure: true
+})
 
 export async function POST(req) {
   const formData = await req.formData();
 
   if (formData.has('file')) {
     const file = formData.get('file');
-    const fileBuffer = await file.arrayBuffer();
-
-
+    const fileBuffer = await file.arrayBuffer(); // Convert File object to a buffer
+    // console.log(fileBuffer)
     try {
-      const uploadResponse = await cloudinary.uploader.upload_stream({}, async (error, result) => {
-        if (error) {
-          console.error('Error uploading to Cloudinary:', error);
-          return new Response('Error uploading image', { status: 500 });
-        }
-        if (result && result.secure_url) {
-          return Response.json(result.secure_url);
-        }
-      }).end(fileBuffer);
+      let mime = file.type;
+      let encoding = 'base64';
+      let base64Data = Buffer.from(fileBuffer).toString('base64');
+      let fileUri = 'data:' + mime + ';' + encoding + ',' + base64Data;
 
-      return uploadResponse; // Return the response from Cloudinary
+      console.log("1----------", fileUri);
+      const uploadToCloudinary = () => {
+        return new Promise((resolve, reject) => {
+
+          let result = cloudinary.uploader.upload(fileUri, {
+            invalidate: true
+          })
+            .then((result) => {
+              console.log(result);
+              resolve(result);
+            })
+            .catch((error) => {
+              console.error("2------------", error);
+              reject(error);
+            });
+        });
+      };
+
+      let uploadResponse = await uploadToCloudinary();
+      console.log("3------------------");
+      console.log(uploadResponse);
+      return Response.json(uploadResponse.secure_url);
     } catch (error) {
       console.error('Error during upload:', error);
       return new Response('Error uploading image', { status: 500 });
     }
+
+
   }
 }
