97157678112688e90c07db6f48666da5dc20d7ee
Altafalam3 <altaf.alam0032@gmail.com>
2023-12-28 15:13:03 +0530

    Trying harder fix

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -14,39 +14,32 @@ export async function POST(req) {
     const file = formData.get('file');
     const fileBuffer = await file.arrayBuffer(); // Convert File object to a buffer
     // console.log(fileBuffer)
-    try {
-      let mime = file.type;
-      let encoding = 'base64';
-      let base64Data = Buffer.from(fileBuffer).toString('base64');
-      let fileUri = 'data:' + mime + ';' + encoding + ',' + base64Data;
 
-      console.log("1----------", fileUri);
-      const uploadToCloudinary = () => {
-        return new Promise((resolve, reject) => {
+    let mime = file.type;
+    let encoding = 'base64';
+    let base64Data = Buffer.from(fileBuffer).toString('base64');
+    let fileUri = 'data:' + mime + ';' + encoding + ',' + base64Data;
 
-          let result = cloudinary.uploader.upload(fileUri, {
-            invalidate: true
+    const uploadToCloudinary = () => {
+      return new Promise((resolve, reject) => {
+
+        let result = cloudinary.uploader.upload(fileUri, {
+          invalidate: true
+        })
+          .then((result) => {
+            console.log(result);
+            resolve(result);
           })
-            .then((result) => {
-              console.log(result);
-              resolve(result);
-            })
-            .catch((error) => {
-              console.error("2------------", error);
-              reject(error);
-            });
-        });
-      };
+          .catch((error) => {
+            console.log(error);
+            reject(error);
+          });
+      });
+    };
 
-      let uploadResponse = await uploadToCloudinary();
-      console.log("3------------------");
-      console.log(uploadResponse);
-      return Response.json(uploadResponse.secure_url);
-    } catch (error) {
-      console.error('Error during upload:', error);
-      return new Response('Error uploading image', { status: 500 });
-    }
+    let uploadResponse = await uploadToCloudinary();
+    console.log(uploadResponse);
 
-
+    return Response.json(uploadResponse.secure_url);
   }
 }
